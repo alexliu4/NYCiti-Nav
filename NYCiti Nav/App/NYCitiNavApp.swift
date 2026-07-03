b60d2de76e305b3a3fb18fcb43cc44905ef5d129
alexrliu <alexrliu@umich.edu>
2026-07-03 01:15:07 -0400

    feat(init): initial commit

---
+++ NYCiti Nav/App/NYCitiNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYCitiNavApp.swift
+//  NYCiti Nav
+//
+//  Created by Alex Liu on 5/19/26.
+//
+
+import SwiftUI
+
+@main
+struct NYCitiNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
